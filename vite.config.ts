2c1f9708a18e8bf697d8109b121ff41f89a9fd3d
NilsonCueva <josenilson.cueva@gmail.com>
2025-09-25 12:01:34 -0500

    Cambios Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,6 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  // 👇 IMPORTANTE: aquí defines el nombre exacto de tu repo
+  base: "/sueldo-pe-magico/",
 }));
